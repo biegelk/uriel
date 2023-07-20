a18baeea4e72f8e94ec96c615d99c29b8385fc16
biegelk <biegelk3@gmail.com>
2023-07-20 16:55:45 -0500

    setting up a log-file to compensate for inability to print debug information, and automatically clearing the logfile at the start of each new run of the program

--- data_manager.py
+++ data_manager.py
@@ -1,20 +1,15 @@
+import os
 import pandas
 import yaml
 
 
 class DataManager(object):
     def __init__(self, file_dict):
-        if "map" in file_dict.keys():
-            map_array = initialize_map(file_dict["map"])
         if "sequences" in file_dict.keys():
             self.stree_data = self.initialize_sequences(file_dict["sequences"])
-
-
-
-    def initialize_map(self, map_file):
-        map_array = None
-
-        return map_array
+        if "log" in file_dict.keys():
+            self.logfile = file_dict["log"]
+            self.initialize_logfile()
 
 
     def initialize_sequences(self, sequences_file):
@@ -24,3 +19,11 @@ class DataManager(object):
                      )
 
         return stree_data
+
+
+    def initialize_logfile(self):
+        if os.path.exists(self.logfile):
+            os.remove(self.logfile)
+
+        with open(self.logfile, "a") as outfile:
+            outfile.write("Log file:\n")
